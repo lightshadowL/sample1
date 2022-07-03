11172f22d4b485007527b2434de90cc3f4610956
jong lee <lsljw94@daum.net>
2022-07-03 13:49:04 +0900

    팝업

--- js/common.js
+++ js/common.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    
+    $(".notic-content>li:first-child").click(function(){
+        $(".modal-wrap").show();
+    })
+    $(".modal-close").click(function(){
+        $(".modal-wrap").hide();
+    });
 });
